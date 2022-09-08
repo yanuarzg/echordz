0263a1242d916f6b73962d1fd89f9e17c927983c
yanuabialwi <zhalxdev@gmail.com>
2022-09-08 09:03:09 +0700

    Create m.js

---
+++ m.js
@@ -0,0 +1,1 @@
+let EFV='<a href="https://www.catatandroid.com" style="position:absolute;z-index:-999;left:-99%;">CatatanDroid</a>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
